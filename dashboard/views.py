64e709a4bf940f872be6765b96103db411aec9e9
mudit mahajan <muditmhjn99@gmail.com>
2020-07-25 19:28:58 +0530

    class based dash views added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from firebase_utils import app
+import firebase_utils
 
 # Create your views here.
-def dashboard(request):
-    org_ref = app.fetch_firestore('organizations')
-    return render(request, 'dashboard.html', info)
 
 
 def createorg(request):
     return render(request, 'createorg.html', {})
 
+class DashDisplay(object):
+    def __init__(self):
+        self.app = firebase_utils.FirebaseSDK()
+        self.db_dealer = firebase_utils.Deals()
 
-def postcreateorg(request):
-    name = request.POST.get('name')
-    details = {'name':name, 'user': 'mud'}
-    app.add_to_firestore(details, 'organizations')
-    return HttpResponse("org created")
+    def dashboard(self, request, user_uid):
+        user_ref = self.app.fetch_firestore('user', user_uid)
+        user_dict = user_ref.get().to_dict()
+        # user_dict : {
+        #             'name': name,
+        #             'email': email,
+        #             'password': password,
+        #             'contact': contact,
+        #             'organizations': []
+        #         }
+        return render(request, 'dashboard.html', user_dict)
+
+    def displayorgs(self, request):
+        pass
+
+    def postcreateorg(self, request):
+        name = request.POST.get('name')
+        details = {'name':name, 'user': 'mud'}
+        self.app.add_to_firestore(details, 'organizations')
+        return HttpResponse("org created")
 
 
 
