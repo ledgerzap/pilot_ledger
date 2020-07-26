70955d152494c045ef8b1a476261fa851e4ce13d
mudit mahajan <muditmhjn99@gmail.com>
2020-07-26 18:14:48 +0530

    urls updates

--- pilot_ledger/urls.py
+++ pilot_ledger/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage ,name="homepage"),
+    path('', views.homepage, name="homepage"),
     path('authenticate/', views.authenticate, name="loginauth"),
     path('signup/', views.signup, name="signup_form"),
     path('postsignup/', views.postsignup, name="postsignup"),
